f65c661618a23f2fcae57258394f80e33fec9e72
Sion <solee9802@gmail.com>
2024-08-21 22:56:32 +0900

    work list 등록 중간 커밋 - 작업 내역 팝업에서 부모로 카테고리명, 차수, 가격 전달 - 결제 완료 및 작업 완료 값 저장

--- BueatyLine/src/components/CategoryWork.jsx
+++ BueatyLine/src/components/CategoryWork.jsx
@@ -1,28 +1,52 @@
 import { useEffect, useState } from 'react'
 import './CategoryWork.scss'
 
-const CategoryWork = ({category_name, category_cha, category_price, onClose, getChaValue}) => {
+const CategoryWork = ({
+        category_name, // 부모에게 전달받은 각 인덱스 모달에 뜰 카테고리 명
+        category_cha, // 카테고리 차수
+        category_price, // 가격
+        getCategoryName, // 부모에게 전달 할 카테고리 명
+        getChaValue, // 부모에게 전달할 차수
+        getCategoryPrice, // 부모에게 전달할 가격
+        onClose, // 닫기 버튼 클릭 시
+        onConfirmClose // 확인 버튼 클릭 시
+    }) => {
     // 가격 변경 시 수정될 금액
-    const [price, setPrice] = useState()
+    const [price, setPrice] = useState(category_price || '')
     const onChangePrice = (e) => {
         setPrice(e.target.value)
     }
 
     // 버튼 클릭 시 해당 버튼 value 값 담고 active 클래스 추가
-    const [activeIndex, setActiveIndex] = useState(null); // 버튼 class 핸들링
+    const [activeIndex, setActiveIndex] = useState(0); // 버튼 class 핸들링
     const [btnValue, setBtnValue] = useState(1) // value 초기값 1
 
-    const getBtnValue = (e, index) => { // 버튼 클릭 시
-        setBtnValue(e.target.value)
+    // 버튼 클릭 시
+    const getBtnValue = (e, index) => { 
+        const value = e.target.value || e.currentTarget.value;
+        setBtnValue(value)
         setActiveIndex(index)
     }
-    useEffect(() => {
-        
-    }, [btnValue])
-    const pushValues = (value) => {
-        getChaValue(value)
+
+    // 카테고리 명 전달
+    const getName = (category_name) => {
+        getCategoryName(category_name)
+    }
+
+    // 가격 전달
+    const getPrice = (price) => {
+        getCategoryPrice(price)
+    }
+    
+    const pushValues = () => {
+        // 카테고리 명 전달
+        getName(category_name)
+        // 차수 값 전달
+        getChaValue(btnValue)
+        // 가격 전달
+        getPrice(price)
         // 팝업 닫기
-        onClose()
+        onConfirmClose()
     }
 
     return (
--- BueatyLine/src/pages/AddWorkList.jsx
+++ BueatyLine/src/pages/AddWorkList.jsx
@@ -5,29 +5,15 @@ import axios from 'axios';
 import CategoryWork from '../components/CategoryWork';
 
 const AddWorkList = () => {
-    // 작업일 
-    const [workDate, setWorkDate] = useState('');
-    const changeWorkDate = (e) => {
-        setWorkDate(e.target.value);
-    };
+    // 카테고리 api 호출
 
     const [list, setList] = useState([]); // api list를 담을 state
     const accessToken = localStorage.getItem('accessToken'); // api 인가용 aceessToken값
 
-    // 카테고리 api 호출
     const categoryApiList = async () => {
         try {
             const url = '/api/work_api/getCategory';
-            // const params = {
-            //     work_date: workDate,
-            //     idx_kmc_member: '',
-            //     work_memo: '',
-            //     work_price_completed: '',
-            //     work_completed: '',
-            //     cha_values: ''
-            // };
             const response = await axios.get(url, {
-                // params: params,
                 headers: {
                     'Authorization': `Bearer ${accessToken}`
                 }
@@ -52,43 +38,35 @@ const AddWorkList = () => {
         }
     };
 
-    // 작업 내역 등록 api
-    const [chaValues, setChaValues] = useState() // 차수값
-    const getChaValue = (data) => {
-        setChaValues(data)
-    }
-    const addWorkApiList = async () => {
-        try{
-            const url = '/api/work_api/add';
-            let params = {
-                work_idx: '',
-                work_date: workDate,
-                idx_kmc_member: customerName,
-                cha_values: chaValues,
-                prices: '',
-                work_memo: '',
-                work_price_completed: '',
-                work_completed: ''
-            }
-            const response = await axios.get(url, {
-                params: params,
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            })
-            console.log('날짜 ' + workDate)
-            console.log('차수 ' + chaValues)
-            return response.data;
-        }catch(error){
-            console.error(error);
-            console.log('날짜 ' + workDate)
-            console.log('차수 ' + chaValues)
-        }
+    // 작업 등록
+    // 작업 날짜 
+    const [workDate, setWorkDate] = useState('');
+    const changeWorkDate = (e) => {
+        setWorkDate(e.target.value);
+    };
+
+    // 작업 내역
+    // 작업 이름값
+    const [categoryName, setCategoryName] = useState('')
+    const getCategoryName = (data) => {
+        setCategoryName(data)
     }
 
-    useEffect(() => {
-        getCategoryList();
-    }, []);
+    // 작업 차수값
+    const [chaValues, setChaValues] = useState([]) 
+    const getChaValue = (data) => {
+        // setChaValues({
+        //     ...chaValues,
+        //     data: [...(chaValues.data || []), ...data]
+        // });
+        setChaValues(data)
+    }
+
+    // 작업 가격
+    const [price, setPrice] = useState()
+    const getCategoryPrice = (data) => {
+        setPrice(data)
+    }
 
     // 작업(카테고리) 선택 체크
     const [isChecked, setIsChecked] = useState([]); // 체크 O/X 표시
@@ -113,8 +91,6 @@ const AddWorkList = () => {
     }
 
     useEffect(() => {
-        getChaValue()
-        console.log(chaValues)
         checkCloseBtn(isChecked)
     }, [isChecked])
 
@@ -124,12 +100,91 @@ const AddWorkList = () => {
         setCustomerName(e.target.value);
     };
 
+    // 결제 완료 체크
+    const [activePay, setActivePay] = useState(false); // 버튼 class 핸들링
+    const [payValue, setPayValue] = useState(0)
+    const activePayBtn = () => {
+        setActivePay(true)
+    }
+    const inActivePayBtn = () => {
+        setActivePay(false)
+    }
+    const getPayValue = () => {
+        if(activePay === true) {
+            setPayValue(1)
+        }
+        if(activePay === false) {
+            setPayValue(0)
+        }
+    };
+
+    // 작업 완료 체크
+    const [activeWork, setActiveWork] = useState(false); // 버튼 class 핸들링
+    const [workValue, setWorkValue] = useState(0)
+    const activeWorkBtn = () => {
+        setActiveWork(true)
+    }
+    const inActiveWorkBtn = () => {
+        setActiveWork(false)
+    }
+    const getWorkValue = () => {
+        if(activeWork === true) {
+            setWorkValue(1)
+        }
+        if(activeWork === false) {
+            setWorkValue(0)
+        }
+    };
+    useEffect(() => {
+        getPayValue()
+        getWorkValue()
+    }, [activePay, activeWork])
+
     // 메모 입력
     const [memo, setMemo] = useState('');
     const changeMemo = (e) => {
         setMemo(e.target.value);
     };
 
+    // 작업 내역 등록 api
+    const addWorkApiList = async () => {
+        try{
+            const url = '/api/work_api/add';
+            let params = {
+                work_idx: '',
+                work_date: workDate, // 작업 날짜
+                idx_kmc_member: customerName, // 고객이름
+                categories_name : categoryName, // 작업명
+                cha_values: chaValues, // 작업 차수
+                prices: price, // 가격
+                work_memo: '', // 메모
+                work_price_completed: payValue, // 결제 완료(1) / 미완료 표시(0) 
+                work_completed: workValue // 결제 완료(1) / 미완료 표시(0) 
+            }
+            const response = await axios.get(url, {
+                params: params,
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            })
+            console.log('날짜 ' + workDate)
+            console.log('카테고리명 ' + categoryName)
+            console.log('차수 ' + chaValues)
+            console.log(activePay)
+            console.log(response)
+            return response.data;
+        }catch(error){
+            console.error(error);
+            console.log('날짜 ' + workDate)
+            console.log('카테고리명 ' + categoryName)
+            console.log('차수 ' + chaValues)
+        }
+    }
+
+    useEffect(() => {
+        getCategoryList();
+    }, []);
+    
     return (
         <div className="AddWorkList position-relative">
             <div className='subTitle'>Work List 등록</div>
@@ -140,7 +195,7 @@ const AddWorkList = () => {
                 </section>
                 <section className='mb-4'>
                     <h6 className='fw-bold'>작업 내역</h6>
-                    <div className='row flex-direction-column'>
+                    <div className='row flex-direction-column mb-3'>
                         {list.map((item, index) => (
                             <div key={index} className='border-bottom border-1 pt-1 pb-1'>
                                 <label>
@@ -156,14 +211,30 @@ const AddWorkList = () => {
                                         category_name = {item.category_name}
                                         category_cha = {item.category_cha}
                                         category_price = {item.category_price}
-                                        checkCloseBtn = {checkCloseBtn}
-                                        onClose={() => onChangeCheck({ target: { checked: false } }, index)}
+                                        getCategoryName = {getCategoryName}
                                         getChaValue = {getChaValue}
+                                        getCategoryPrice = {getCategoryPrice}
+                                        onClose={() => onChangeCheck({ target: { checked: false } }, index)}
+                                        onConfirmClose={() => onChangeCheck({ target: { checked: true } }, index)}
+                                        checkCloseBtn = {checkCloseBtn}
                                     />
                                 }
                             </div>
                         ))}
                     </div>
+                    {
+                        isChecked &&
+                        <article className='mb-4'>
+                            <h6 className='fw-bold'>작업 내역 확인</h6>
+                            <div className='row align-items-center w-auto p-0 m-0'>
+                                <div className='w-auto p-0 me-1'>{categoryName}</div>
+                                <div className='badge rounded-pill bg-info d-block w-auto'>{chaValues}차</div>
+                            </div>
+                            <div>
+                                가격: {price}원
+                            </div>
+                        </article>
+                    }
                 </section>
                 <section className='mb-4'>
                     <div className='row flex-direction-column'>
@@ -174,13 +245,13 @@ const AddWorkList = () => {
                 <section className='mb-4'>
                     <h6 className='fw-bold'>결제 완료</h6>
                     <div className='row mb-3'>
-                        <button type='button' className='w-50 active'>완료</button>
-                        <button type='button' className='w-50'>미완료</button>
+                        <button type='button' className={`w-50 p-0 ${activePay === true ? 'active' : ''}`} onClick={activePayBtn}>완료</button>
+                        <button type='button' className={`w-50 p-0 ${activePay === false ? 'active' : ''}`} onClick={inActivePayBtn}>미완료</button>
                     </div>
                     <h6 className='fw-bold'>작업 완료</h6>
                     <div className='row'>
-                        <button type='button' className='w-50'>완료</button>
-                        <button type='button' className='w-50 active'>미완료</button>
+                        <button type='button' className={`w-50 p-0 ${activeWork === true ? 'active' : ''}`} onClick={activeWorkBtn}>완료</button>
+                        <button type='button' className={`w-50 p-0 ${activeWork === false ? 'active' : ''}`} onClick={inActiveWorkBtn}>미완료</button>
                     </div>
                 </section>
                 <section className='mb-4'>
